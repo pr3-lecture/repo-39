f6ffd17d7241a086c4a3e75d2059272cec0a30f3
Stella Neser <stella.neser@googlemail.com>
2017-11-29 22:29:53 +0100

    verbesserte Version

--- lisp/Uebungsaufgabe1.lisp
+++ lisp/Uebungsaufgabe1.lisp
@@ -1,12 +1,12 @@
 ;gnu clisp 2.49
-;@author: Stella Neser
 
 (print "Lisp, Uebungsblatt 1!")
-(setq x (list 1 2 3 '(4 5) 6))
+
+
 
 ;Aufgabe 1
 ;a)
-(defun rotieren (x)
+(defun rotieren (x) 
     "setze das erste element ans ende"
     (append (rest x) (list (first x)))
     )
@@ -29,7 +29,7 @@
 
 (defun my-length (a)
     "Laenge einer list inklusive der geschachtelten listen"
-    (cond ((null a) 0)
+    (cond ((null a) 0) 
         (t (+ 1 (my-length(rest a))))))
 
 ;d)
@@ -54,17 +54,79 @@
     )
 )
 
+;fuer aufgabe 1
+(setq x (list 1 2 3 '(4 5) 6))
+;fuer aufgabe 2
+(setq tree1 '(10 (5 (3) (7)) (13 (12) (20))))
+;Aufgabe2
+;a)
+;B-Baeume sind oft verwendete unterarten von Baeumen. Die Knoten eines B-Baumes
+;koennen nur max 2 Nachkommen (elemente) haben. Er besteht jewehls aus 3 
+;Elementen; Vater, linkes Kind, rechtes Kind. Jedes Kind kann auch ein Vater
+;sein => Aufbau wird rekursiv fortgefuehrt. Man kann jedes element als Liste 
+;darstellen => Listern werden ineinander verzweigt, wie in einem Baum
 
-;a test
+;b)
+;b.1) preorder (tree)
+(defun preorder (tree)
+    "PREORDER Output"
+    (cond
+        ((null tree) nil)
+        ((listp (car tree)) (preorder (car tree)) (preorder (cdr tree)))
+        (t (princ (car tree)) (princ " ") (preorder (cdr tree)))
+    )
+)    
+
+;b.2) inorder (tree)
+(defun inorder (tree)
+    "INORDER Output"
+    (cond
+        ( ( null tree) '() )
+        ( (atom (car tree) ) (inorder (cdr tree))(princ (car tree)) (princ " ") (inorder (cddr tree)))
+        (t (inorder (car tree)))
+    )
+)
+
+;b.3) preorder (tree)
+(defun postorder (tree)
+    "POSTORDER Output"
+    ;(rotieren tree)
+    (inorder (cdr tree))
+    (inorder (cddr tree))
+    (princ (car tree))
+)
+
+
+(print "===")
+(print "Aufgabe 1")
+(print "===")
+(print "")
+;1a test
 (print (rotieren x))
-;b test
+;1b test
 (print (neues-vorletztes x 4))
-;c test
+;1c test
 (print (my-length x))
-;d test
+;1d test
 (print (my-lengthR x))
-;e test
+;1e test
 (print (my-reverse x))
-;f test
+;1f test
 (print (my-reverseR x))
-
+(print "")
+(print "===")
+(print "Aufgabe 2")
+(print "===")
+(print tree1)
+(print "")
+;2a test
+;2b test
+;2b1 test
+(preorder tree1)
+(print "")
+;(print (cdr tree1))
+(inorder tree1)
+;(print (rest tree1))
+;(inorder (rest tree1))
+(print "")
+(postorder tree1)
