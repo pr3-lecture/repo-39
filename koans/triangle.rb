053a69663753f7dd7b14328e2acfaa81521310f1
Stella Neser <stella.neser@googlemail.com>
2017-12-07 11:11:55 +0100

    179 about_sandwich_code

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = [a,b,c].sort
+
+   if (a<=0) || (b<=0) ||( c<=0)
+     raise TriangleError
+   elsif (a+b <= c)
+     raise TriangleError
+   end
+
+   return :equilateral if a==c
+   return :isosceles if a==b or b==c
+   :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
